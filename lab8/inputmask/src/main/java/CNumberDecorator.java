4e036eea870f662c54dd910aaa9b5e04e0e6d36b
kavyadayanand <kavya.d.dayanand@gmail.com>
2019-04-06 21:48:05 -0700

    Update CNumberDecorator.java

--- lab8/inputmask/src/main/java/CNumberDecorator.java
+++ lab8/inputmask/src/main/java/CNumberDecorator.java
@@ -5,18 +5,8 @@ public class CNumberDecorator implements IDecorator{
 
 	@Override
 	public String decorate(String str) {
-//		String number = str;
-//		for(int i=0;i<str.length();i++){
-//			if(i==4||i==8||i==12){
-//				number = number+" "+str.charAt(i);
-//			}else
-//			{
-//				number = str+str.charAt(i);
-//			}
-//		}
-//		return number;
 		
-		StringBuilder cardNumber = new StringBuilder();
+		StringBuffer cardNumber = new StringBuffer();
 		for(int i=0;i<str.length();i++) {
 			if(i==4 || i==8 || i==12)
 				cardNumber.append(" " + str.charAt(i));
