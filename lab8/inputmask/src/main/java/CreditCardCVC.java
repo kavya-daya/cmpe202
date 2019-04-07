de9f76e391539f433db88624b1d42a6f982427c1
kavyadayanand <kavya.d.dayanand@gmail.com>
2019-04-06 21:48:59 -0700

    Update CreditCardCVC.java

--- lab8/inputmask/src/main/java/CreditCardCVC.java
+++ lab8/inputmask/src/main/java/CreditCardCVC.java
@@ -18,12 +18,6 @@ public class CreditCardCVC implements IDisplayComponent, IKeyEventHandler
 	}	
 
 	public void key(String ch, int cnt) {
-//		if ( cnt >= 21 ) {
-//			if ( cnt >= 21 && cnt <= 23 )
-//				cvc += ch ;
-//			else if ( nextHandler != null )
-//				nextHandler.key(ch, cnt) ;
-//		}
 		if(cnt>=21 && cnt<=23) {
 			if(ch.matches("x|X"))
 				cvc = cvc.substring(0, cvc.length() - 1);
